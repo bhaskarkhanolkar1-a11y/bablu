a6a632fa310ea67ec470eb773cd4e412b6761ae1
bhaskarkhanolkar1-a11y <bhaskar.khanolkar1@gmail.com>
2025-08-12 12:49:41 -0700

    input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,36 +3,10 @@ import * as React from "react";
 function cn(...classes: Array<string | false | null | undefined>) {
 	return classes.filter(Boolean).join(" ");
 }
-
-
-export import * as React from "react";
-
-function cn(...classes: Array<string | false | null | undefined>) {
-	return classes.filter(Boolean).join(" ");
-}
-
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 
 export interface InputProps
-	extends React.InputHTMLAttributes<HTMLInputElement>;
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	({ className, type = "text", ...props }, ref) => {
-		return (
-			<input
-				type={type}
-				className={cn(
-					"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground/20 disabled:cursor-not-allowed disabled:opacity-50",
-					className
-				)}
-				ref={ref}
-				{...props}
-			/>
-		);
-	}
-);
-Input.displayName = "Input";
-
-export { Input };
+	extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
 	({ className, type = "text", ...props }, ref) => {

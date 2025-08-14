137f5073086200dd794f942c39b506b76ebf9101
bhaskarkhanolkar1-a11y <bhaskar.khanolkar1@gmail.com>
2025-08-14 13:55:00 -0700

    Add files via upload

---
+++ components/BarcodeScanner.tsx
@@ -0,0 +1,57 @@
+// FILE: components/BarcodeScanner.tsx
+
+"use client";
+
+import {
+	Html5QrcodeScanner,
+	Html5QrcodeSupportedFormats,
+} from "html5-qrcode";
+import type { QrcodeSuccessCallback } from "html5-qrcode/esm/core";
+import * as React from "react";
+
+const qrcodeRegionId = "html5qr-code-full-region";
+
+interface BarcodeScannerProps {
+	onScanSuccess: QrcodeSuccessCallback;
+	onScanFailure?: (error: string) => void;
+}
+
+export function BarcodeScanner({ onScanSuccess }: BarcodeScannerProps) {
+	React.useEffect(() => {
+		const formatsToSupport = [
+			Html5QrcodeSupportedFormats.QR_CODE,
+			Html5QrcodeSupportedFormats.EAN_13,
+			Html5QrcodeSupportedFormats.UPC_A,
+			Html5QrcodeSupportedFormats.UPC_E,
+			Html5QrcodeSupportedFormats.CODE_128,
+		];
+
+		const scanner = new Html5QrcodeScanner(
+			qrcodeRegionId,
+			{
+				qrbox: {
+					width: 250,
+					height: 250,
+				},
+				fps: 10,
+				rememberLastUsedCamera: true,
+				formatsToSupport: formatsToSupport,
+			},
+			/* verbose= */ false
+		);
+
+		// The fix is changing 'error' to '_error' below
+		scanner.render(onScanSuccess, _error => {
+			// The library will log errors, we can ignore them here.
+		});
+
+		// Cleanup function to stop the scanner when the component unmounts
+		return () => {
+			scanner.clear().catch(error => {
+				console.error("Failed to clear html5-qrcode scanner. ", error);
+			});
+		};
+	}, [onScanSuccess]);
+
+	return <div id={qrcodeRegionId} />;
+}
